6ad8e802df6d4aadb13c041421472ebc64a037bc
Husein <nvdhunter007@gmail.com>
2020-07-03 01:11:52 +0700

    add employee entity and repository

---
+++ src/employee/employee.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  Unique,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
+import bcrypt from 'bcrypt';
+
+@Entity()
+@Unique(['username'])
+export class Employee extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  private password: string;
+
+  @Column()
+  salt: string;
+
+  async setPassword(password: string): Promise<void> {
+    this.salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(password, this.salt);
+  }
+
+  async validatePassowrd(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
+}
