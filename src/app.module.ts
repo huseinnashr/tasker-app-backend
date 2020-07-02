538bc3c6dc1c8bd00cfed72cf63798c1045fb493
Husein <nvdhunter007@gmail.com>
2020-07-02 21:42:05 +0700

    init auth & employee module, ctrl, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, EmployeeModule],
 })
 export class AppModule {}
