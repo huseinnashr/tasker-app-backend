180391e879cfb7f09614f0b1fc91b06f8f90605c
Husein <nvdhunter007@gmail.com>
2020-07-08 20:32:23 +0700

    test forbidden support passing token

--- test/helper/test.helper.ts
+++ test/helper/test.helper.ts
@@ -23,20 +23,30 @@ class TestHelper {
   };
 
   /** Create new employee with role R and test if endpoint forbid role R employee.*/
-  forbidden = async (role: Role, method: HTTPMethod, url: string) => {
-    const signUpDTO = { username: 'testforbidden', role };
-    const [auth] = await this.signUp(signUpDTO);
+  async forbidden(role: Role, method: HTTPMethod, url: string);
+
+  /** Use token with role R to test if endpoint forbid role R employee.*/
+  async forbidden(token: string, method: HTTPMethod, url: string);
+
+  async forbidden(arg1: Role | string, method: HTTPMethod, url: string) {
+    let token: string;
+    if (Object.values(<any>Role).includes(arg1)) {
+      const signUpDTO = { username: 'testforbidden', role: <Role>arg1 };
+      token = (await this.signUp(signUpDTO))[0];
+    } else {
+      token = arg1;
+    }
 
     await this.supertest
       .request(method, url)
-      .set({ Authorization: auth })
+      .set({ Authorization: token })
       .expect(403);
-  };
+  }
 
-  /** Create new employee with role R and test if endpoint returns not found.*/
+  /** Create new employee with correct role and test if endpoint returns not found.*/
   async notfound(role: Role, method: HTTPMethod, url: string, data?: any);
 
-  /** Use token to test if endpoint returns not found.*/
+  /** Use token  with correct role to test if endpoint returns not found.*/
   async notfound(token: string, method: HTTPMethod, url: string, data?: any);
 
   async notfound(
