2dcc44b0220d06856f0a11f3a42f4d8ebf14f479
Husein <nvdhunter007@gmail.com>
2020-07-18 18:04:57 +0700

    setup swagger and the cli plugin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import env from 'ts-get-env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Tasker App')
+    .setDescription('The Tasker App API Documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(env.get('number', 'PORT', 3000));
 }
 bootstrap();
