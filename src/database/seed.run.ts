9fa831adab81accaef1a238b2cf529739f663feb
Husein <nvdhunter007@gmail.com>
2020-07-30 00:02:20 +0700

    seeding debug log

--- src/database/seed.run.ts
+++ src/database/seed.run.ts
@@ -3,13 +3,17 @@ import { ConnectionOptions, getConnectionManager } from 'typeorm';
 import * as TypeOrmConfig from '../config/typeorm.config';
 
 const seedRun = async () => {
+  console.debug('Creating a DB connection...');
   const connManager = getConnectionManager();
   const connection = connManager.create(TypeOrmConfig as ConnectionOptions);
   await connection.connect();
 
+  console.debug('Running the DB seeders...');
   const databaseSeeder = new DatabaseSeed();
   databaseSeeder.setConnection(connection);
   await databaseSeeder.run();
+
+  console.debug('Seeding DONE!');
 };
 
 seedRun().then();
